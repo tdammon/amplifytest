a4bebac9e00d3d7bea9a5eb25faff9cbe486bbf6
The Goose <trevordammon@gmail.com>
2020-04-27 15:56:49 -0500

    setup

---
+++ src/App/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import {isMobile, isBrowser} from 'react-device-detect';
+import Mobile from "../Mobile/Mobile";
+import MobileServices from "../MobileServices/MobileServices";
+import MobileNavThreeButton from "../MobileNavThreeButton/MobileNavThreeButton";
+import MobileNavFourButton from "../MobileNavFourButton/MobileNavFourButton";
+import MobileNavFiveButton from "../MobileNavFiveButton/MobileNavFiveButton";
+import Desktop from "../Desktop/Desktop";
+import DesignView from "../DesignView/DesignView"
+
+
+class App extends Component {
+
+  render() {
+    if(isMobile){
+      return(
+      <Router>
+        <Switch>
+        <Route
+          exact
+          path="/"
+          component={Mobile}
+        />
+        <Route
+          exact
+          path="/services"
+          component={MobileServices}
+        />  
+        <Route
+          exact
+          path="/designs"
+          component={DesignView}
+        />  
+        </Switch>
+        <MobileNavFourButton />
+      </Router>)
+    } else if(isBrowser) {
+      return (
+        <Router>
+          <Switch>
+          <Route
+              exact
+              path="/"
+              component={Desktop}
+            />
+          </Switch>
+        </Router>
+      );
+    }
+    
+  }
+}
+
+export default (App);
