8e386324a7839deb1281a4119af76cb71d3bae65
The Goose <trevordammon@gmail.com>
2020-04-27 16:19:24 -0500

    test

--- src/App/App.js
+++ src/App/App.js
@@ -1,51 +1,12 @@
 import React, { Component } from 'react';
-import {
-  HashRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-} from 'react-router-dom';
-import {isMobile, isBrowser} from 'react-device-detect';
+
 
 
 
 class App extends Component {
 
   render() {
-    if(isMobile){
-      return(
-      <Router>
-        {/* <Switch>
-        <Route
-          exact
-          path="/"
-          component={Mobile}
-        />
-        <Route
-          exact
-          path="/services"
-          component={MobileServices}
-        />  
-        <Route
-          exact
-          path="/designs"
-          component={DesignView}
-        />  
-        </Switch> */}
-      </Router>)
-    } else if(isBrowser) {
-      return (
-        <Router>
-          {/* <Switch>
-          <Route
-              exact
-              path="/"
-              component={Desktop}
-            />
-          </Switch> */}
-        </Router>
-      );
-    }
+ <div>Hello</div>
     
   }
 }
